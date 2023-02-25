a89a3ac20a74a6a567b2d95031aa80950a5102ae
Anzhelika <mrstonem@mail.ru>
2023-02-25 12:34:02 +0300

    add react-router dom, suspencce, lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from "./components/Counter";
+import { Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { Suspense } from "react";
+import { MainAsync }  from "./pages/Main/Main.async";
+import { AboutAsync }  from "./pages/About/About.async";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      "sfsfsfdsdf"
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainAsync />}/>
+          <Route path={"/about"} element={<AboutAsync />}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      PAGE ABOUT!
+    </div>
+  );
+}
+
+export default About;
